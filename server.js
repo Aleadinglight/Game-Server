d200b8fc78a6406fbe4940bdcaac7688ad5b191a
unknown <huutam.ptnk@gmail.com>
2018-12-22 12:12:11 +0200

    added socket.io and http server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');  
+var server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
@@ -13,4 +15,4 @@ app.get('/', (req, res) => {
 app.get('*', (req, res, next) => res.redirect('/'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+server.listen(port, () => console.log(`Listening on port ${port}...`));
